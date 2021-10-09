4c55b9d083644ed3a63ecf39716380a2ae80fa57
kwhn <kwhn9486@naver.com>
2021-10-09 12:36:08 +0900

    Controlview & Storyboard interlock problem solved

--- iOS_Study/ViewController.swift
+++ iOS_Study/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for : UIControl.State.normal)
+            button.backgroundColor = UIColor.orange
+        } else {
+            button.setTitle(emoji, for : UIControl.State.normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
+    
 }
 
